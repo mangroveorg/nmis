998351b5f826024830c7e5c586ac211329d088ee
Alex Dorey <dorey415@gmail.com>
2011-04-21 17:10:23 -0400

    commented out supplemental STATICFILES_DIRS setting

--- settings.py
+++ settings.py
@@ -79,9 +79,7 @@ STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    os.path.join(CURRENT_DIR, 'static'),
-)
+#STATICFILES_DIRS = ('/other/static/files',)
 
 # List of finder classes that know how to find static files in
 # various locations.
