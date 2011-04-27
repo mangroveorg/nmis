b3d2157f28d179758a34c3eb1383059fe494264f
Alex Dorey <dorey415@gmail.com>
2011-04-27 13:33:01 -0400

    sorting variables by "display_order".

--- main/static/js/src/display_mdg_table.js
+++ main/static/js/src/display_mdg_table.js
@@ -62,7 +62,8 @@
 				thR.append($("<td />", {'class': mH + ' year'}).text(year))
 			});
 			tb.append(thR);
-			$.each(goals[''+gn], function(ii, gvar){
+			var goalsInDisplayOrder = goals[''+gn].sort(function(a,b){return a.display_order - b.display_order;})
+			$.each(goalsInDisplayOrder, function(ii, gvar){
 				tb.append(createRowForVariable(gvar))
 			})
 		});
