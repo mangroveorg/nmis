7eeb5a12867f49b161e0dd5a307fde2b7d0de9b9
rgaudin <rgaudin@gmail.com>
2011-05-02 22:40:58 +0000

    example of retrieving name of Entities in templates

--- main/widget_data.py
+++ main/widget_data.py
@@ -5,13 +5,17 @@ The widget_ids for each level are set in widgets.py
 """
 
 def country_view(entity):
-    return {u'entity_id': entity.id}
+    return {u'entity_id': entity.id,
+            u'name': entity.aggregation_paths['_geo'][-1]}
 
 def lga_view(entity):
-    return {u'entity_id': entity.id}
+    return {u'entity_id': entity.id,
+            u'name': entity.aggregation_paths['_geo'][-1]}
 
 def state_view(entity):
-    return {u'entity_id': entity.id}
+    return {u'entity_id': entity.id,
+            u'name': entity.aggregation_paths['_geo'][-1]}
 
 def mdg_table(entity):
-    return {u'entity_id': entity.id}
+    return {u'entity_id': entity.id,
+            u'name': entity.aggregation_paths['_geo'][-1]}
