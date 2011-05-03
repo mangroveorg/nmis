4922f0a71c1d3e3c56014b46a50f3a392a18e8c4
Alex Dorey <dorey415@gmail.com>
2011-05-03 10:15:56 -0400

    packaged stuff in the widgets a little nicer.

--- main/widget_data.py
+++ main/widget_data.py
@@ -1,21 +1,28 @@
 """
-This packages up a dict which is passed to the template in BASE_TEMPLATES/widgets/widget_id
+These methods are intended to package up a dict which is passed to the template in BASE_TEMPLATES/widgets/widget_id.html
 
-The widget_ids for each level are set in widgets.py
+The widget_ids for each level are set in widgets.py.
 """
 
-def country_view(entity):
-    return {u'entity_id': entity.id,
+def _packaged_dict_for_entity(entity):
+    """
+    This can be reused with commonly repeated values for the widget.
+    """
+    return {u'entity_id': entity.id, \
             u'name': entity.aggregation_paths['_geo'][-1]}
 
-def lga_view(entity):
-    return {u'entity_id': entity.id,
-            u'name': entity.aggregation_paths['_geo'][-1]}
+def country_view(entity, region_thing):
+    d = _packaged_dict_for_entity(entity)
+    return d
 
-def state_view(entity):
-    return {u'entity_id': entity.id,
-            u'name': entity.aggregation_paths['_geo'][-1]}
+def lga_view(entity, region_thing):
+    d = _packaged_dict_for_entity(entity)
+    return d
 
-def mdg_table(entity):
-    return {u'entity_id': entity.id,
-            u'name': entity.aggregation_paths['_geo'][-1]}
+def state_view(entity, region_thing):
+    d = _packaged_dict_for_entity(entity)
+    return d
+
+def mdg_table(entity, region_thing):
+    d = _packaged_dict_for_entity(entity)
+    return d
