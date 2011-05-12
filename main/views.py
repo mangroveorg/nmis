e473a6fdc1a4e6da1458fef79b28392a13c7ac24
Alex Dorey <dorey415@gmail.com>
2011-05-12 11:14:33 -0400

    nmis: removed stuff about displaying spreadsheets (old spike)

--- main/views.py
+++ main/views.py
@@ -10,7 +10,6 @@ from main.region_thing import RegionThing, import_region_thing_from_dict
 from helpers import read_required
 import json
 import widgets
-import spreadsheet_display
 import os
 NIGERIA_REGION_CACHE = 'nigeria_regions.json'
 
@@ -41,23 +40,6 @@ def region_navigation(request, region_path):
         except:
             context.__dict__[widget_id] = False
     return render_to_response("region_navigation.html", context_instance=context)
-
-
-def spreadsheet_json(request, sheet_name):
-    "This is pulled by JSON and handled in the page."
-    output = spreadsheet_display.for_display(sheet_name)
-    # if display_method=="django_template":
-    #     context = RequestContext(request)
-    #     context.spreadsheet_name = output.get(u'name')
-    #     output[u'html'] = \
-    #         template_loader.render_to_string("spreadsheet_partial.html", context_instance=context)
-    return HttpResponse(json.dumps(output))
-
-
-def spreadsheets(request):
-    context = RequestContext(request)
-    context.spreadsheet_types = spreadsheet_display.LISTS
-    return render_to_response("spreadsheets.html", context_instance=context)
 
 
 def region_root_object():
