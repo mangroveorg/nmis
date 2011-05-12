e2208a30e3675fb2a159bbed9f040d6f0479ca1c
rgaudin <rgaudin@gmail.com>
2011-05-11 21:37:28 -0400

    added import pictures command

--- local_settings_example.py
+++ local_settings_example.py
@@ -1,3 +1,7 @@
+
+import os
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 #this should be unique
 SECRET_KEY = 'tHiSiSnOtUnIqUew0)_d6az7=^vhffs9sn0zq!g(aopz(x&mzl'
 
@@ -19,3 +23,6 @@ ACCOUNT_ACTIVATION_DAYS = 5
 
 # where to redirect after login
 LOGIN_REDIRECT_URL = '/'
+
+# where massive pictures are stored
+PICTURES_FOLDER = '%s/pictures/' % CURRENT_DIR
