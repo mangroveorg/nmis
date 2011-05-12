d02c0ef3c7bac6d57c15a75efe6af887342b6b4e
rgaudin <rgaudin@gmail.com>
2011-05-12 15:26:28 -0400

    typo

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ import views
 
 urlpatterns = patterns('',
     url(r'^profiles/(?P<region_path>\S*)', views.region_navigation),
-    url(r'^LGAs.json?', views.lgas_json, name='lgas-list'),
+    url(r'^LGAs.json', views.lgas_json, name='lgas-list'),
 )
