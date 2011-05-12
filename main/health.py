d41317c93d308862ee02901db9b955cb44c14e5f
Andrew Marder <andrew.ei.marder@gmail.com>
2011-05-12 08:24:36 -0700

    Got first draft of health scores finished.

--- main/health.py
+++ main/health.py
@@ -54,6 +54,52 @@ def physical_condition(data):
         return 1
     return 0
 
+def convert_to_number(string):
+    try:
+        f = float(string)
+        i = int(string)
+        if i == f:
+            return i
+        return f
+    except:
+        return 0
+
+def at_least_one_doctor_nurse_or_midwife(data):
+    keys = ["num_doctors_fulltime", "num_midwives_fulltime", "num_nurses_fulltime"]
+    counts = [convert_to_number(data[key]) for key in keys]
+    if counts == [0, 0, 0]:
+        return 0
+    if len([count for count in counts if count >= 1]) > 0:
+        return 1
+    return None
+
+def at_least_one_chew(data):
+    count = convert_to_number(data["num_chews_fulltime"])
+    if count is None:
+        return None
+    if count >= 1:
+        return 1
+    return 0
+
+def at_least_one_technician_if_facility_has_lab(data):
+    if data["laboratory_yn"] == "no":
+        return None
+    if convert_to_number(data["num_lab_techs_fulltime"]) >= 1:
+        return 1
+    return 0
+
+def no_user_fees_for_maternal_health_services(data):
+    keys = [
+        "paid_services_routine_antenatal_visit",
+        "paid_services_contraceptives",
+        "paid_services_anc_delivery",
+        ]
+    fees = [data[key] for key in keys]
+    if fees == ["FALSE", "FALSE", "FALSE"]:
+        return 1
+    return 0
+
+
 scores = [
     {
         "name": 'Access',
@@ -86,47 +132,74 @@ scores = [
             physical_condition,
             ]
         },
+    {
+        "name": 'Staffing',
+        "slug": "staffing",
+        "primitive_type": 'formula',
+        "description": 'Staffing score for health facility.',
+        "components": [
+            at_least_one_doctor_nurse_or_midwife,
+            at_least_one_chew,
+            at_least_one_technician_if_facility_has_lab,
+            ("staff_quarters_yn", "equals", "yes"),
+            ("days_facility_understaffed", "less than", 7),
+            ("staff_paid_lastmth_yn", "equals", "yes"),
+            ]
+        },
+    {
+        "name": 'Services Provided (Child Health)',
+        "slug": "child_services",
+        "primitive_type": 'formula',
+        "description": 'Services Provided (Child Health)',
+        "components": [
+            ("child_health_srvcs_bcg_immunization", "equals", "TRUE"),
+            ("child_health_srvcs_growth_monitoring", "equals", "TRUE"),
+            ("malaria_treatment_yn", "equals", "TRUE"),
+            ("child_health_srvcs_dewormig", "equals", "TRUE"),
+            ("paid_services_child_health_services", "equals", "FALSE"),
+            ]
+        },
+    {
+        "name": 'Services Provided (Maternal Health)',
+        "slug": "maternal_services",
+        "primitive_type": 'formula',
+        "description": 'Services Provided (Maternal Health)',
+        "components": [
+            ("antenatal_care_yn", "equals", "yes"),
+            ("family_planning_yn", "equals", "yes"),
+            ("hiv_treatment_srvcs_pmtct_services", "equals", "TRUE"),
+            ("emergency_obstetrics_yn",  "equals", "yes"),
+            ("emergency_transport_ambulance", "equals", "TRUE"),
+            no_user_fees_for_maternal_health_services,
+            ]
+        },
+    {
+        "name": 'Services Provided (HIV/TB)',
+        "slug": "hiv_services",
+        "primitive_type": 'formula',
+        "description": 'Services Provided (HIV/TB)',
+        "components": [
+            ("hiv_treatment_srvcs_counselors", "equals", "FALSE"),
+            ("hiv_treatment_srvcs_pmtct_services", "equals", "TRUE"),
+            ("hiv_treatment_yn", "equals", "yes"),
+            ("lab_tests_performed_tb_microscopy", "equals", "TRUE"),
+            ("tb_treatment_yn", "equals", "yes"),
+            ]
+        },
+    {
+        "name": 'Equipment / Supplies',
+        "slug": "equipment_supplies",
+        "primitive_type": 'formula',
+        "description": 'Equipment / Supplies',
+        "components": [
+            ("xray_funct_yn", "equals", "yes"),
+            ("ultra_sound_funct_yn", "equals", "yes"),
+            ("refrigerator_funct_yn", "equals", "yes"),
+            ("emergency_transport_ambulance", "equals", "TRUE",),
+            ("equipment_available_operating_surgical_theatre", "equals", "TRUE"),
+            ("laboratory_yn", "equals", "yes"),
+            # 'No stockouts >1 week in past 3 months',
+            ("antimalarials_stockout_yn", "equals", "no"),
+            ]
+        },
     ]
-
-
-    # 'Staffing': [
-    #     '>1 Doctor/nurse/midwife',
-    #     '>1 CHEW',
-    #     'Lab technician (for facililties with labs)',
-    #     'Living quarters',
-    #     'Not understaffed >1 wk in past month',
-    #     'Staff paid in past month',
-    #     ],
-    # 'Services Provided (Child Health)': [
-    #     'Immunization',
-    #     'Growth monitoring',
-    #     'Malaria treatment',
-    #     'Deworming',
-    #     'No user fees for child health services',
-    #     ],
-    # 'Services Provided (Maternal Health)': [
-    #     'Antenatal care',
-    #     'Family planning',
-    #     'PMTCT program',
-    #     'Comprehensive emergency obstetric care',
-    #     'Ambulance',
-    #     'No user fees for maternal health services',
-    #     ],
-    # 'Services Provided (HIV/TB)': [
-    #     'VCT, PMTCT, HIV treatment, TB treatment',
-    #     'VCT',
-    #     'PMTCT',
-    #     'HIV TREATMENT',
-    #     'TB testing',
-    #     'TB treatment',
-    #     ],
-    # 'Equipment / Supplies': [
-    #     'Xray machine',
-    #     'Ultrasound',
-    #     'Refrigerator',
-    #     'Ambulance',
-    #     'Operating theatre',
-    #     'Laboratory',
-    #     'No stockouts >1 week in past 3 months',
-    #     ],
-    # }
