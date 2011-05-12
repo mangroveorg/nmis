99d0b65b9afc851fe42bafa96872c5fd35097747
Alex Dorey <dorey415@gmail.com>
2011-05-12 18:28:44 -0400

    added slug to mdg indicators

--- main/raw_mdg_indicator_list.py
+++ main/raw_mdg_indicator_list.py
@@ -25,30 +25,30 @@ will generate a list in this format--
 #Spreadsheet located in dropbox:
 # Dropbox/NMIS - Nigeria/MDG Indicator List/MDG_consolidated_list_v2.xls
 pasted_from_spreadsheet = """
-YES	YES	Education	Enrollment rates	2	Net enrollment ratio for primary education  	HNLSS
-YES	YES	Education	Enrollment rates	2	Gross enrollment ratio in primary education  	HNLSS
-NO	YES	Education	Enrollment rates	2	Net enrollment ratio for secondary education	HNLSS
-NO	YES	Education	Enrollment rates	2	Gross enrollment ratio for secondary education  	HNLSS
-YES	YES	Education	Transition rates	2	Transition rate for females from primary to JS1 	School Census 2005
-YES	YES	Education	Transition rates	2	Transition rate for males from primary to JS1  	School Census 2005
-YES	YES	Education	Repetition rates	2	Percent Male repeaters in primary school 	School Census 2005
-YES	YES	Education	Repetition rates	2	Percent Female repeaters in primary school 	School Census 2005
-YES	YES	Education	Literacy rate	2	Literacy rate (Percent) 	HNLSS 
-YES	YES	Gender	Student Flows	3	Ratio of girls to boys in primary schools  	?
-YES	YES	Health	Child Health	4	Immunization rate (DPT 3) 	DHS 2008
-NO	YES	Health	Health Provider	4	Skilled health provider: population ratio 	(facility inventories)
-NO	YES	Health	Health Provider	4	Midwife: population ratio 	(facility inventories)
-YES	YES	Health	Child Health	4	Prevalence of underweight (weight-for-age) in children under 5 years (-3SD from mean Z-score) 	DHS 2008
-NO	YES	Health	Health Provider	4	CHW: population ratio 	(facility inventories)
-YES	YES	Health	Child Health	4	Prevalence of stunting (height-for-age) in children under 5 years (-3SD from mean Z-score) 	DHS 2008
-YES	YES	Health	Child Health	4	Prevalence of wasting (weight-for-height) in children under 5 years (-3SD from the mean Z-scores) 	DHS 2008
-YES	YES	Health	Child Health	4	Under 5 mortality rate 	(DHS 2008)
-YES	YES	Health	Maternal Health	5	Contraceptive prevalence rate among married or in-union women aged 15-49 years  [modern methods]  	DHS 2008 [from table]
-YES	YES	Health	Maternal Health	5	Proportion of births attended by skilled health personnel 	HNLSS 2009
-YES	YES	Health	HIV & AIDS	6	Percentage of women that have been tested for HIV ever 	DHS 2008
-YES	YES	Health	HIV & AIDS	6	Percentage of men that have been tested for HIV ever 	DHS 2008
-YES	YES	Infrastructure	Water access	7	Percentage households with access to improved water sources	?
-YES	YES	Infrastructure	Sanitation access	7	Percentage households with access to improved sanitation	?
+i1	YES	YES	Education	Enrollment rates	2	Net enrollment ratio for primary education  	HNLSS
+i2	YES	YES	Education	Enrollment rates	2	Gross enrollment ratio in primary education  	HNLSS
+i3	NO	YES	Education	Enrollment rates	2	Net enrollment ratio for secondary education	HNLSS
+i4	NO	YES	Education	Enrollment rates	2	Gross enrollment ratio for secondary education  	HNLSS
+i5	YES	YES	Education	Transition rates	2	Transition rate for females from primary to JS1 	School Census 2005
+i6	YES	YES	Education	Transition rates	2	Transition rate for males from primary to JS1  	School Census 2005
+i7	YES	YES	Education	Repetition rates	2	Percent Male repeaters in primary school 	School Census 2005
+i8	YES	YES	Education	Repetition rates	2	Percent Female repeaters in primary school 	School Census 2005
+i9	YES	YES	Education	Literacy rate	2	Literacy rate (Percent) 	HNLSS 
+i10	YES	YES	Gender	Student Flows	3	Ratio of girls to boys in primary schools  	?
+i11	YES	YES	Health	Child Health	4	Immunization rate (DPT 3) 	DHS 2008
+i12	NO	YES	Health	Health Provider	4	Skilled health provider: population ratio 	(facility inventories)
+i13	NO	YES	Health	Health Provider	4	Midwife: population ratio 	(facility inventories)
+i14	YES	YES	Health	Child Health	4	Prevalence of underweight (weight-for-age) in children under 5 years (-3SD from mean Z-score) 	DHS 2008
+i15	NO	YES	Health	Health Provider	4	CHW: population ratio 	(facility inventories)
+i16	YES	YES	Health	Child Health	4	Prevalence of stunting (height-for-age) in children under 5 years (-3SD from mean Z-score) 	DHS 2008
+i17	YES	YES	Health	Child Health	4	Prevalence of wasting (weight-for-height) in children under 5 years (-3SD from the mean Z-scores) 	DHS 2008
+i18	YES	YES	Health	Child Health	4	Under 5 mortality rate 	(DHS 2008)
+i19	YES	YES	Health	Maternal Health	5	Contraceptive prevalence rate among married or in-union women aged 15-49 years  [modern methods]  	DHS 2008 [from table]
+i20	YES	YES	Health	Maternal Health	5	Proportion of births attended by skilled health personnel 	HNLSS 2009
+i21	YES	YES	Health	HIV & AIDS	6	Percentage of women that have been tested for HIV ever 	DHS 2008
+i22	YES	YES	Health	HIV & AIDS	6	Percentage of men that have been tested for HIV ever 	DHS 2008
+i23	YES	YES	Infrastructure	Water access	7	Percentage households with access to improved water sources	?
+i24	YES	YES	Infrastructure	Sanitation access	7	Percentage households with access to improved sanitation	?
 """
 
 def convert_pasted_data():
@@ -56,13 +56,14 @@ def convert_pasted_data():
    q = [l.split("\t") for l in lines]
    r = []
    for d in q:
-       o = {'country_display': d[0]=="YES",
-           'lga_display': d[1]=="YES",
-           'sector': d[2],
-           'subsector': d[3],
-           'goal_number': int(d[4]),
-           'name': d[5].strip(),
-           'data_source': d[6].strip()
+       o = {'slug': d[0],
+            'country_display': d[1]=="YES",
+           'lga_display': d[2]=="YES",
+           'sector': d[3],
+           'subsector': d[4],
+           'goal_number': int(d[5]),
+           'name': d[6].strip(),
+           'data_source': d[7].strip()
            }
        r.append(o)
    return r
