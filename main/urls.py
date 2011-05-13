fefc2645850ed1d530c297908d63ad9ecd121607
rgaudin <rgaudin@gmail.com>
2011-05-12 21:15:20 -0400

    removed blank home page

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, url
 import views
 
 urlpatterns = patterns('',
+    url(r'^/?$', views.region_navigation, {'region_path': 'nigeria'}),
     url(r'^profiles/(?P<region_path>\S*)', views.region_navigation),
     url(r'^LGAs/', views.lgas_json, name='lgas-list'),
 )
--- urls.py
+++ urls.py
@@ -6,16 +6,9 @@ admin.autodiscover()
 from main.views import main as main_index
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'nmis.views.home', name='home'),
-    # url(r'^nmis/', include('nmis.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^/?$', main_index),
     url(r'', include('nmis.main.urls')),
     url(r'^indicators/', include('nmis.indicator_management.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
