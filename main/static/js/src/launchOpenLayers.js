950eed2b2310738c3ea827556db456fe63834e95
Alex Dorey <dorey415@gmail.com>
2011-05-31 12:00:48 -0400

    nmis: re-jigged layer configuration so we can move it out of this file

--- main/static/js/src/launchOpenLayers.js
+++ main/static/js/src/launchOpenLayers.js
@@ -12,6 +12,7 @@ var LaunchOpenLayers = (function (wrapId, _opts) {
       localTiles: false,
       tileUrl: "http://tilestream.openmangrove.org:8888/",
       tileCache: "http://localhost:8000/tiles/",
+      layers: [["Nigeria", "nigeria_base"]],
       zoom: 6
   }, opts = $.extend({}, defaultOpts, _opts);
 
@@ -35,112 +36,45 @@ var LaunchOpenLayers = (function (wrapId, _opts) {
     ) 
   };
   
+  opts.layers = [
+    ["Nigeria", "nigeria_base"],
+    ["Nigeria health workers per thousand people", "nigeria_healthworkers_per_thousand"],
+    ["Nigeria Health Facilities with Institutional Deliveries", "pct_healthfacilities_with_institutional_delivery"],
+    ["Nigeria No Stockouts of Bednets or Malaria Medicine", "nigeria_pct_no_bednet_malmeds_oneweek"],
+    ["Nigeria Classrooms That Need Repair", "nigeria_pct_classroom_need_repair"],
+    ["Nigeria classrooms with proportion student to teacher ratio > 40", "nigeria_prop_ratio_greater_than_40"],
+    ["Nigeria Immunization Rate", "nigearia_immunization_rate"],
+    ["Nigeria Under 5 Mortality Rate", "nigeria_under5_mortality_rate"],
+    ["Nigeria Child Wasting", "nigeria_wasting"],
+    ["Nigeria Child Health", "nigeria_child_health"],
+    ["Nigeria Child Nutrition", "nigeria_child_nutrition"],
+    ["Nigeria Maternal Health", "nigeria_maternal_health"],
+    ["Nigeria Primary Education Enrollment", "nigeria_primary_education_enrollment"]
+  ];
+  
   map = new OpenLayers.Map(wrapId, options);
   var mapserver = !!opts.localTiles ? 
                     opts.tileCache : opts.tileUrl;
-  var nigeria = new OpenLayers.Layer.TMS(
-    "Nigeria",
-    [ mapserver],
-    { 'layername': 'nigeria_base', 'type': 'png' }
-  );
-
-    
-
-  var nigeria_healthworkers_per_thousand  = new OpenLayers.Layer.TMS(
-    "Nigeria health workers per thousand people",
-    [mapserver],
-    {'layername': 'nigeria_healthworkers_per_thousand', 'type': 'png'}
-  );
-
-  var pct_healthfacilities_with_institutional_delivery  = new OpenLayers.Layer.TMS(
-    "Nigeria Health Facilities with Institutional Deliveries",
-    [mapserver],
-    {'layername': 'pct_healthfacilities_with_institutional_delivery', 'type': 'png'}
-  );
-
-  var nigeria_pct_no_bednet_malmeds_oneweek  = new OpenLayers.Layer.TMS(
-    "Nigeria No Stockouts of Bednets or Malaria Medicine",
-    [mapserver],
-    {'layername': 'nigeria_pct_no_bednet_malmeds_oneweek', 'type': 'png'}
-  );
-
-  var nigeria_pct_classroom_need_repair  = new OpenLayers.Layer.TMS(
-    "Nigeria Classrooms That Need Repair",
-    [mapserver],
-    {'layername': 'nigeria_pct_classroom_need_repair', 'type': 'png'}
-  );
-
-  var nigeria_prop_ratio_greater_than_40  = new OpenLayers.Layer.TMS(
-    "Nigeria classrooms with proportion student to teacher ratio > 40",
-    [mapserver],
-    {'layername': 'nigeria_prop_ratio_greater_than_40', 'type': 'png'}
-  );
-
-
-  var nigearia_immunization_rate = new OpenLayers.Layer.TMS(
-    "Nigeria Immunization Rate",
-    [mapserver],
-    {'layername': 'nigearia_immunization_rate', 'type': 'png'}
-  );
-
-  var nigeria_under5_mortality_rate = new OpenLayers.Layer.TMS(
-    "Nigeria Under 5 Mortality Rate",
-    [ mapserver],
-    { 'layername': 'nigeria_under5_mortality_rate', 'type': 'png' }
-  );
-
-  var nigeria_wasting = new OpenLayers.Layer.TMS(
-    "Nigeria Child Wasting",
-    [ mapserver],
-    { 'layername': 'nigeria_wasting', 'type': 'png' }
-  );
-
-  var nigeria_child_health = new OpenLayers.Layer.TMS(
-    "Nigeria Child Health",
-    [mapserver],
-    {'layername': 'nigeria_child_health', 'type': 'png'}
-  );
-  var nigeria_child_nutrition = new OpenLayers.Layer.TMS(
-    "Nigeria Child Nutrition",
-    [mapserver],
-    {'layername': 'nigeria_child_nutrition', 'type': 'png'}
-  );
-
-  var nigeria_malaria = new OpenLayers.Layer.TMS(
-    "Nigeria Malaria",
-    [mapserver],
-    {'layername': 'nigeria_malaria', 'type': 'png'}
-  );
-
-  var nigeria_maternal_health = new OpenLayers.Layer.TMS(
-    "Nigeria Maternal Health",
-    [mapserver],
-    {'layername': 'nigeria_maternal_health', 'type': 'png'}
-  );
-
-
-  var nigeria_primary_education_enrollment = new OpenLayers.Layer.TMS(
-    "Nigeria Primary Education Enrollment",
-    [mapserver],
-    {'layername': 'nigeria_primary_education_enrollment', 'type': 'png'}
-  );
-
- var gsat = new OpenLayers.Layer.Google(
-    "Google Satellite",
-    {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22}
-  );
-
-  var gphy = new OpenLayers.Layer.Google(
-        "Google Physical",
-    {type: google.maps.MapTypeId.TERRAIN}
-  );
-
-    map.addLayers([nigeria, nigeria_healthworkers_per_thousand, pct_healthfacilities_with_institutional_delivery, nigeria_pct_no_bednet_malmeds_oneweek, nigeria_pct_classroom_need_repair, nigeria_prop_ratio_greater_than_40, nigearia_immunization_rate, nigeria_child_health, nigeria_child_nutrition, nigeria_malaria, nigeria_maternal_health, nigeria_wasting, nigeria_under5_mortality_rate, nigeria_primary_education_enrollment, gsat, gphy]);
-
+                    
+  var mapLayers = $.map(opts.layers, function(i, ldata){
+      return new OpenLayers.Layer.TMS(ldata[0], [mapserver], 
+          {
+              layername: ldata[1],
+              'type': 'png'
+          });
+  });
+  mapLayers.push(new OpenLayers.Layer.Google(
+      "Google Satellite",
+      {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22}
+      ));
+  mapLayers.push(new OpenLayers.Layer.Google(
+      "Google Physical",
+      {type: google.maps.MapTypeId.TERRAIN}
+      ));
+  
+  map.addLayers(mapLayers);
   map.addControl(new OpenLayers.Control.LayerSwitcher());
-
-  var center = new OpenLayers.LonLat(opts.centroid.lng, opts.centroid.lat);
-  map.setCenter(center, opts.zoom)
+  map.setCenter(new OpenLayers.LonLat(opts.centroid.lng, opts.centroid.lat), opts.zoom)
 
     $('#layer-select').change(function(param) {
 
