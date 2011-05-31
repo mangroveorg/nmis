09bfd6a71829c0fc203b096383221008f9316997
Alex Dorey <dorey415@gmail.com>
2011-05-31 16:39:16 -0400

    nmis: moved layer specification code out of main method into "defaults".

--- main/static/js/src/launchOpenLayers.js
+++ main/static/js/src/launchOpenLayers.js
@@ -1,3 +1,19 @@
+var defaultLayers = [
+    ["Nigeria", "nigeria_base"],
+    ["Nigeria health workers per thousand people", "nigeria_healthworkers_per_thousand"],
+    ["Nigeria Health Facilities with Institutional Deliveries", "pct_healthfacilities_with_institutional_delivery"],
+    ["Nigeria No Stockouts of Bednets or Malaria Medicine", "nigeria_pct_no_bednet_malmeds_oneweek"],
+    ["Nigeria Classrooms That Need Repair", "nigeria_pct_classroom_need_repair"],
+    ["Nigeria classrooms with proportion student to teacher ratio > 40", "nigeria_prop_ratio_greater_than_40"],
+    ["Nigeria Immunization Rate", "nigearia_immunization_rate"],
+    ["Nigeria Under 5 Mortality Rate", "nigeria_under5_mortality_rate"],
+    ["Nigeria Child Wasting", "nigeria_wasting"],
+    ["Nigeria Child Health", "nigeria_child_health"],
+    ["Nigeria Child Nutrition", "nigeria_child_nutrition"],
+    ["Nigeria Maternal Health", "nigeria_maternal_health"],
+    ["Nigeria Primary Education Enrollment", "nigeria_primary_education_enrollment"]
+  ];
+
 var LaunchOpenLayers = (function (wrapId, _opts) { 
   var wrap = $('#'+wrapId);
   var defaultOpts = {
@@ -10,9 +26,10 @@ var LaunchOpenLayers = (function (wrapId, _opts) {
       boundingBox: false,
       mapHeight: 475,
       localTiles: false,
+      layerSelector: '#layer-select',
       tileUrl: "http://tilestream.openmangrove.org:8888/",
       tileCache: "http://localhost:8000/tiles/",
-      layers: [["Nigeria", "nigeria_base"]],
+      layers: defaultLayers,
       zoom: 6
   }, opts = $.extend({}, defaultOpts, _opts);
 
@@ -35,22 +52,6 @@ var LaunchOpenLayers = (function (wrapId, _opts) {
       20037500
     ) 
   };
-  
-  opts.layers = [
-    ["Nigeria", "nigeria_base"],
-    ["Nigeria health workers per thousand people", "nigeria_healthworkers_per_thousand"],
-    ["Nigeria Health Facilities with Institutional Deliveries", "pct_healthfacilities_with_institutional_delivery"],
-    ["Nigeria No Stockouts of Bednets or Malaria Medicine", "nigeria_pct_no_bednet_malmeds_oneweek"],
-    ["Nigeria Classrooms That Need Repair", "nigeria_pct_classroom_need_repair"],
-    ["Nigeria classrooms with proportion student to teacher ratio > 40", "nigeria_prop_ratio_greater_than_40"],
-    ["Nigeria Immunization Rate", "nigearia_immunization_rate"],
-    ["Nigeria Under 5 Mortality Rate", "nigeria_under5_mortality_rate"],
-    ["Nigeria Child Wasting", "nigeria_wasting"],
-    ["Nigeria Child Health", "nigeria_child_health"],
-    ["Nigeria Child Nutrition", "nigeria_child_nutrition"],
-    ["Nigeria Maternal Health", "nigeria_maternal_health"],
-    ["Nigeria Primary Education Enrollment", "nigeria_primary_education_enrollment"]
-  ];
   
   map = new OpenLayers.Map(wrapId, options);
   var mapserver = !!opts.localTiles ? 
@@ -76,36 +77,36 @@ var LaunchOpenLayers = (function (wrapId, _opts) {
   map.addControl(new OpenLayers.Control.LayerSwitcher());
   map.setCenter(new OpenLayers.LonLat(opts.centroid.lng, opts.centroid.lat), opts.zoom)
 
-    $('#layer-select').change(function(param) {
-
-        layer_id = $(this).val();
-        layer = eval(layer_id);
-
-        layer.map.setBaseLayer(layer);
-
-        $('.mapped-indicator').hide();
-        $('#mapped-'+layer_id).show();
-    });  
-
-    if(!!opts.points) {
-        var markers = new OpenLayers.Layer.Markers( "Markers" );
-        map.addLayer(markers);
-        
-        var icon = (function(){
-            var size = new OpenLayers.Size(21,25);
-            var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-            return new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
-        })();
+  if(!!opts.layerSelector) {
+      $(opts.layerSelector).change(function(param){
+          layer_id = $(this).val();
+          layer = eval(layer_id);
+          layer.map.setBaseLayer(layer);
+          $('.mapped-indicator').hide();
+          $('#mapped-'+layer_id).show();
+      })
+  }
+  if(!!opts.points) {
+      var markers = new OpenLayers.Layer.Markers("Markers");
+      map.addLayer(markers);
+      var icon = (function(pointData){
+          var iconType = "health"; // for example
+          if(icons[iconType]===undefined) {
+                var iconUrl = 'http://www.openlayers.org/dev/img/marker.png';
+                var size = new OpenLayers.Size(21,25);
+                var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+                icons[iconType] = new OpenLayers.Icon(iconUrl), size, offset);
+            }
+            return icons[iconType];
+        });
         
         var bounds = new OpenLayers.Bounds();
-        var lls = [];
     	$.each(opts.points, function(i, d){
     		var _ll = d.latlng.split(",");
     		var ll = [+_ll[0], +_ll[1]];
             bounds.extend(new OpenLayers.LonLat(ll[1], ll[0]));
-            markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(ll[1], ll[0]), icon));
+            markers.addMarker(new OpenLayers.Marker(new OpenLayers.LonLat(ll[1], ll[0]), icon(d)));
     	});
-    	
         map.zoomToExtent(bounds);
     }
-})
+});
